b97a74b93aed73232ca259a0dd24a6c441909cb0
Friedrich Laudanum <niobiumz41@gmail.com>
2019-04-14 15:19:38 +0200

    [ADD] 'Love it\!' rating counter and icons

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../models/post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-list-item',
@@ -12,7 +13,7 @@ export class PostListItemComponent implements OnInit {
 
   private postDate: Date;
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
     this.postDate = new Date(this.post.createdAt);
@@ -20,10 +21,21 @@ export class PostListItemComponent implements OnInit {
 
   onLoveIt(): void {
     this.post.loveIts++;
+    this.postService.savePosts();
   }
 
   onDontLoveIt(): void {
     this.post.loveIts--;
+    this.postService.savePosts();
+  }
+
+  displayLoveItScore(): string {
+    if (this.post.loveIts >= 0) {
+      return this.post.loveIts.toString() + ' 👍';
+    }
+    else {
+      return -this.post.loveIts.toString() + ' 👎';
+    }
   }
 
 }
